4b468285937f4d9c55837227ce69e0b9f8ba3466
JuampaVLB <juampablovillalba25@gmail.com>
2023-01-29 02:38:03 -0300

    update

--- src/componentes/Login.jsx
+++ src/componentes/Login.jsx
@@ -1,15 +1,50 @@
 import styles from "../modulos/Login.module.css"
 import libro from "../assets/img/libro.png"
+import { authApi } from '../api/auth.api';
 import { useState } from "react";
-
+import axios from "axios";
+import Swal from 'sweetalert2'
 
 function Login(){
   
+
+ 
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [dni, setDni] = useState('');
   const [datos, setDatos] = useState({
     username: ' ' ,
     password: ' ' ,
-      dni: ' '
+    dni: ' '
   })
+
+  function Signin(username, password, dni) {
+
+    axios.post('http://localhost:4000/api/auth/signin', {
+      username,
+      password,
+      dni
+    })
+    .then((result) => {
+      window.location = "home";
+    })
+    .catch((err) => {
+      Swal.fire({
+        title: 'Error!',
+        text: 'Usuario Incorrecto',
+        icon: 'error',
+        confirmButtonText: 'OK'
+      })
+    })
+
+    setUsername('');
+    setPassword('');
+    setDni('');
+
+  }
+
+
+
 
   const funcionForm = (event) => {
       setDatos({
@@ -22,7 +57,14 @@ function Login(){
     event.preventDefault();
         const datos = new FormData(event.target);
            const datosCompletos = Object.fromEntries(datos.entries());
-}
+               //console.log(datosCompletos);
+              
+               Signin(username, password, dni);
+
+//               window.location = "home";
+              
+
+              }
 
   return (
       //Formulario de login
@@ -35,18 +77,21 @@ function Login(){
          <section className={styles.inputs}>
            <input name="username"
             placeholder="Ingrese su nombre "
-            onChange={funcionForm}
+            onChange={e => setUsername(e.target.value)}
+            value={username}
             />
 
            <input name="password"
             placeholder="Ingrese su contraseña" 
             type="password"
-            onChange={funcionForm}
+            onChange={e => setPassword(e.target.value)}
+            value={password}
             />
 
            <input name="dni" 
            placeholder="Ingrese su DNI"
-           onChange={funcionForm}
+           onChange={e => setDni(e.target.value)}
+           value={dni}
            />
 
          </section>
@@ -56,6 +101,7 @@ function Login(){
             </button>
          </section>
        </form>
+       <script src="./main.js"></script>
       </>
     )
   }
